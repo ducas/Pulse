494031518ef446835eaece8d6f21af39cd7ee876
Ducas Francis <ducas@ducasfrancis.com>
2012-07-17 12:19:22 +1000

    moved MaintenanceController to its own file under Core

--- Web/Controllers/BusinessEntitiesController.cs
+++ Web/Controllers/BusinessEntitiesController.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web.Mvc;
 using AutoMapper;
-using Kendo.Mvc.UI;
 using Web.Core;
 using Web.Data;
 using Web.Models.BusinessEntities;
@@ -50,92 +47,6 @@ namespace Web.Controllers
             return broker
                 .Execute<CreateBusinessEntityRequest, CreateBusinessEntityResponse>(new CreateBusinessEntityRequest { Entity = model.Map<BusinessEntity>() })
                 .Errors;
-        }
-    }
-
-    public abstract class MaintenanceController<TModel> : Controller where TModel : class
-    {
-        public ActionResult Index()
-        {
-            return PartialOrFullView(null, "_Index");
-        }
-
-        public JsonResult IndexJson(DataSourceRequest request)
-        {
-            var items = GetList(request.Page, request.PageSize);
-            return Json(new { Data = items, Total = items.Count() }, JsonRequestBehavior.AllowGet);
-        }
-
-        protected abstract IEnumerable<object> GetList(int page, int pageSize);
-
-        public ActionResult View(int id)
-        {
-            return PartialOrFullView(Get(id));
-        }
-
-        [HttpGet]
-        public ActionResult Edit(int id)
-        {
-            return View(Get(id));
-        }
-
-        [HttpPost]
-        public ActionResult Edit(TModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var result = Update(model);
-                if (result == null || !result.Any()) return JsonOrRedirect("Index");
-
-                foreach (var e in result) ModelState.AddModelError(e.Key, e.Value);
-            }
-            return JsonErrorsOrView(model);
-        }
-
-        protected abstract IEnumerable<KeyValuePair<string, string>> Update(TModel model);
-
-        protected abstract TModel Get(int id);
-
-        [HttpGet]
-        public ActionResult Create()
-        {
-            return PartialOrFullView(null);
-        }
-
-        [HttpPost]
-        public ActionResult Create(TModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var result = CreateItem(model);
-                if (result == null || !result.Any()) return JsonOrRedirect("Index");
-
-                foreach (var e in result) ModelState.AddModelError(e.Key, e.Value);
-            }
-            return JsonErrorsOrView(model);
-        }
-
-        protected abstract IEnumerable<KeyValuePair<string, string>> CreateItem(TModel model);
-
-        private ActionResult JsonOrRedirect(string action)
-        {
-            return Request.IsAjaxRequest() ? Json(new { success = true, redirect = Url.Action(action) }) : RedirectToAction(action) as ActionResult;
-        }
-
-        private ActionResult JsonErrorsOrView(object model)
-        {
-            return Request.IsAjaxRequest()
-                ? Json(new
-                {
-                    success = false,
-                    errors = ModelState.ToDictionary(k => k.Key, v => v.Value.Errors.Select(e => e.ErrorMessage))
-                })
-                : View(model) as ActionResult;
-        }
-
-        private ActionResult PartialOrFullView(object model, string partial = null)
-        {
-            return Request.IsAjaxRequest() ? PartialView(partial, model) : View(model) as ActionResult;
         }
     }
 }
---
+++ Web/Core/MaintenanceController.cs
@@ -0,0 +1,93 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Kendo.Mvc.UI;
+
+namespace Web.Core
+{
+    public abstract class MaintenanceController<TModel> : Controller where TModel : class
+    {
+        public ActionResult Index()
+        {
+            return PartialOrFullView(null, "_Index");
+        }
+
+        public JsonResult IndexJson(DataSourceRequest request)
+        {
+            var items = GetList(request.Page, request.PageSize);
+            return Json(new { Data = items, Total = items.Count() }, JsonRequestBehavior.AllowGet);
+        }
+
+        protected abstract IEnumerable<object> GetList(int page, int pageSize);
+
+        public ActionResult View(int id)
+        {
+            return PartialOrFullView(Get(id));
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(Get(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = Update(model);
+                if (result == null || !result.Any()) return JsonOrRedirect("Index");
+
+                foreach (var e in result) ModelState.AddModelError(e.Key, e.Value);
+            }
+            return JsonErrorsOrView(model);
+        }
+
+        protected abstract IEnumerable<KeyValuePair<string, string>> Update(TModel model);
+
+        protected abstract TModel Get(int id);
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialOrFullView(null);
+        }
+
+        [HttpPost]
+        public ActionResult Create(TModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = CreateItem(model);
+                if (result == null || !result.Any()) return JsonOrRedirect("Index");
+
+                foreach (var e in result) ModelState.AddModelError(e.Key, e.Value);
+            }
+            return JsonErrorsOrView(model);
+        }
+
+        protected abstract IEnumerable<KeyValuePair<string, string>> CreateItem(TModel model);
+
+        private ActionResult JsonOrRedirect(string action)
+        {
+            return Request.IsAjaxRequest() ? Json(new { success = true, redirect = Url.Action(action) }) : RedirectToAction(action) as ActionResult;
+        }
+
+        private ActionResult JsonErrorsOrView(object model)
+        {
+            return Request.IsAjaxRequest()
+                       ? Json(new
+                           {
+                               success = false,
+                               errors = ModelState.ToDictionary(k => k.Key, v => v.Value.Errors.Select(e => e.ErrorMessage))
+                           })
+                       : View(model) as ActionResult;
+        }
+
+        private ActionResult PartialOrFullView(object model, string partial = null)
+        {
+            return Request.IsAjaxRequest() ? PartialView(partial, model) : View(model) as ActionResult;
+        }
+    }
+}
